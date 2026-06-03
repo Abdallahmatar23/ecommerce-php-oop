44ae8a4c847c7f71a8dab48d87ad0d7fdd9c3864
abdallah <abdallahmatar10@gmail.com>
2026-06-03 16:03:05 +0300

    finish order v1

--- App/views/layout/admin/sidebar.php
+++ App/views/layout/admin/sidebar.php
@@ -274,7 +274,7 @@ function isOpenMenu($keywords, $currentUrl)
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="<?= BASE_URL ?>#">
+              <a class="dropdown-item d-flex align-items-center" href="<?= BASE_URL ?>auth/logout">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
